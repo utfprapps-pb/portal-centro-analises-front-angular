d2c1d9cc4343a5315a425725d80ba4e723d070d6
Lucio <sganzerla.lucio1@gmail.com>
2025-04-17 20:17:10 -0300

    Hotfix: websocket

--- src/app/core/services/websocket.service.ts
+++ src/app/core/services/websocket.service.ts
@@ -27,7 +27,7 @@ export class WebsocketService {
         const token = encodeURIComponent(this.authService.getToken());
         const user = encodeURIComponent(btoa(StorageManager.getItem(Constants.USER)));
 
-        const socket = new WebSocket(`${environment.websocketWSS}://${location.hostname}:443/api/ws?token=${token}&credentials=${user}`);
+        const socket = new WebSocket(`${environment.websocketWSS}://${location.hostname}/api/ws?token=${token}&credentials=${user}`);
         this.stompClient = Stomp.over(socket);
 
         this.stompClient.connect({}, () => {
